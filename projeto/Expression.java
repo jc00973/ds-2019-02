2161e7d6a9c51ffe70289bd4bee367d991e863ca
jc00973 <fonseca.jc@live.com>
2019-11-04 22:39:06 -0200

    Notas aula 04/11.

--- projeto/Expression.java
+++ projeto/Expression.java
@@ -1,8 +1,10 @@
-public class Expression {
+public interface Expression {
 
    private String expression;
 
-   private Map<String, double> values;
+   private Map<String, Double> values;
 
+   public double avalia(Map<String, Double> valores, String expressao);
 
 }
+
