0410ef121ad8153c573b1e7d4dfbe88be34d0f2e
jc00973 <fonseca.jc@live.com>
2019-11-27 15:16:39 -0400

    Implementação do método getExpressaoFor.

--- projeto/src/adaptador/Adapter.java
+++ projeto/src/adaptador/Adapter.java
@@ -8,18 +8,34 @@
  * @author jc00973 - João Carlos Fonseca
  */
 
-package projeto.adaptador.src;
+package projeto.src.adaptador;
 
-public interface Adapter {
+public interface Adapter implements Expressao {
 
     String nome;
     String jarFile;
 
+    /**
+     *  Método construtor padrão.
+     */
     Adapter(String nome, String jarFile) {
-
         this.nome = nome;
         this.jarFile = jarFile;
-
     }
+
+    /**
+     *  Caso o avaliador que será analisado precise implementar um código de preparação, que será executado antes da
+     *  execução da avaliação em si, este deverá constar neste método para que o critério de latência seja analisado.
+     *  A implementação do método em si fica a cargo do utilizador do benchmark.
+     */
+    public void preparar() {};
+
+    /**
+     *  O utilizador do benchmark deverá implementar a forma que irá instanciar uma expressão no benchmark de maneira que
+     *  essa instância possa ser utilizada como parâmetro no projeto que será avaliado.
+     */
+    public Expressao getExpressaoFor(String expressao) {
+        return expr;
+    };
 
 }
