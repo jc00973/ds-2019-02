87b3a6ec179b148d83c944b1edf420e21cc982e3
jc00973 <fonseca.jc@live.com>
2019-11-28 09:21:28 -0400

    Correção do método de instanciação de Adapter.

--- projeto/src/adaptador/Expressao.java
+++ projeto/src/adaptador/Expressao.java
@@ -20,7 +20,7 @@ public interface Expressao {
     *
     * @return O resultado obtido pelo avaliador de expressão.
     */
-   double avalia(Map<String, Double> variaveis);
+   double avaliar(Map<String, Double> variaveis);
 
    /**
     *  Caso o avaliador que será analisado precise implementar um código de preparação, que será executado antes da
@@ -28,12 +28,6 @@ public interface Expressao {
     *  A implementação do método em si fica a cargo do utilizador do benchmark.
     */
    void preparar();
-
-   /**
-    *  O utilizador do benchmark deverá implementar a forma que irá instanciar uma expressão no benchmark de maneira que
-    *  essa instância possa ser utilizada como parâmetro no projeto que será avaliado.
-    */
-   Expressao getExpressaoFor(String expressao);
 
 }
 
