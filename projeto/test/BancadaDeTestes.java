ba143bd4d7b104a8983ad38407f4e2a9e1a1fdda
jc00973 <fonseca.jc@live.com>
2019-11-28 09:19:41 -0400

    Inclusão de classes de teste para arquivo CSV.

---
+++ projeto/test/BancadaDeTestes.java
@@ -0,0 +1,15 @@
+/**
+ * O código abaixo é uma ilustração do design planejado. Ou seja, por se tratar de design, ele serve para orientar e
+ * direcionar o desenvolvedor na construção do software em si, não tendo portanto o intuito de servir como código final
+ * implementável.
+ *
+ */
+
+package projeto.test;
+
+public class BancadaDeTestes implements Supplier {
+
+    public BancadaDeTestes() {
+    }
+
+}
---
+++ projeto/test/Teste.java
@@ -0,0 +1,45 @@
+/**
+ * O código abaixo é uma ilustração do design planejado. Ou seja, por se tratar de design, ele serve para orientar e
+ * direcionar o desenvolvedor na construção do software em si, não tendo portanto o intuito de servir como código final
+ * implementável.
+ *
+ */
+
+package projeto.test;
+
+public class Teste {
+
+    String expressao;
+    Map<String, Double> variaveis;
+    double resultado;
+
+    public Teste(String expressao, Map<String, Double> variaveis, double resultado) {
+        this.expressao = expressao;
+        this.variaveis = variaveis;
+        this.resultado = resultado;
+    }
+
+    public String getExpressao() {
+        return expressao;
+    }
+
+    public void setExpressao(String expressao) {
+        this.expressao = expressao;
+    }
+
+    public Map<String, Double> getVariaveis() {
+        return variaveis;
+    }
+
+    public void setVariaveis(Map<String, Double> variaveis) {
+        this.variaveis = variaveis;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+}
