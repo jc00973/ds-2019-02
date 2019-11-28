928c4a45e266647dc80561e2118b7b3fac29ba08
jc00973 <fonseca.jc@live.com>
2019-11-28 15:10:50 -0400

    Inserção do atributo avaliador.

--- projeto/src/Aplicativo.java
+++ projeto/src/Aplicativo.java
@@ -1,3 +1,6 @@
+import avaliadores.Avaliador;
+import avaliadores.ConsumoMemoria;
+
 /**
  * O código abaixo é uma ilustração do design planejado. Ou seja, por se tratar de design, ele serve para orientar e
  * direcionar o desenvolvedor na construção do software em si, não tendo portanto o intuito de servir como código final
@@ -5,9 +8,8 @@
  *
  */
 
-package projeto;
-
 public class Aplicativo {
 
+        private Avaliador avaliador;
 
-        }
+}
