99aca54fba91c92d2b76d885280660cc38fc4694
jc00973 <fonseca.jc@live.com>
2019-11-29 13:35:37 -0400

    Continuação da implementação de aplicativo.

--- projeto/src/Aplicativo.java
+++ projeto/src/Aplicativo.java
@@ -1,6 +1,3 @@
-import avaliadores.Avaliador;
-import avaliadores.ConsumoMemoria;
-
 /**
  * O código abaixo é uma ilustração do design planejado. Ou seja, por se tratar de design, ele serve para orientar e
  * direcionar o desenvolvedor na construção do software em si, não tendo portanto o intuito de servir como código final
@@ -8,8 +5,39 @@ import avaliadores.ConsumoMemoria;
  *
  */
 
+import avaliadores.Avaliador;
+import avaliadores.ConsumoMemoria;
+
+import java.util.List;
+
 public class Aplicativo {
 
         private Avaliador avaliador;
+        private List<String> resultados;
+
+        /**
+         * Este método recebe um arquivo.txt com 3 informações, respectivamente:
+         *      (a) nome da classe do Avaliador de Expressão que irá instanciar um Adapter;
+         *      (b) caminho com o nome do arquivo.csv contendo testes;
+         *      (c) código de um dos três tipos de avaliação desejado:
+         *              (1) Desempenho;
+         *              (2) Consumo de memória;
+         *              (3) Latência.
+         *      (d) caminho do diretório onde o relatorioResultado.txt deve ser escrito.
+         *
+         */
+        public void insereEntrada() {
+
+                String nomeDaClasse;
+                String caminhoArquivoCsv;
+                String codigoTipoAvaliacao;
+                String caminhoRelatorioResultado;
+
+        };
+
+        public static void main(String[] args) {
+
+        }
+
 
 }
